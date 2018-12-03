3cc0cf4d34a8b8cd0b355afb84035f7dc0902691
pawel717 <psuchanicz@gmail.com>
2018-12-03 20:57:38 +0100

    Add empty activities

---
+++ app/src/main/java/com/pm/pmproject/view/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.pm.pmproject.view.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.pm.pmproject.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
