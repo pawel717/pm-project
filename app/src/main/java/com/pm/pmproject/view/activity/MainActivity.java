d2f87e4a37b9470431f37d94e73bec13f9b8108f
ijuiju71 <m.lapa45@gmail.com>
2018-12-04 17:04:52 +0100

    Stopwatch added

--- app/src/main/java/com/pm/pmproject/view/activity/MainActivity.java
+++ app/src/main/java/com/pm/pmproject/view/activity/MainActivity.java
@@ -1,13 +1,16 @@
 package com.pm.pmproject.view.activity;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-
+import android.view.View;
+import android.widget.Button;
 import com.pm.pmproject.R;
 
 public class MainActivity extends AppCompatActivity {
+    private Button trainingButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +23,15 @@ public class MainActivity extends AppCompatActivity {
         ActionBar actionbar = getSupportActionBar();
         actionbar.setDisplayHomeAsUpEnabled(true);
         actionbar.setHomeAsUpIndicator(R.drawable.ic_menu);
+
+        trainingButton = (Button) findViewById(R.id.button_new_training);
+        trainingButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentNewTraining = new Intent(getBaseContext(), NewWorkoutActivity.class);
+                startActivity(intentNewTraining);
+            }
+        });
 
     }
 }
--- app/src/main/java/com/pm/pmproject/view/activity/NewWorkoutActivity.java
+++ app/src/main/java/com/pm/pmproject/view/activity/NewWorkoutActivity.java
@@ -1,15 +1,46 @@
 package com.pm.pmproject.view.activity;
 
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import android.view.View;
+import android.widget.Chronometer;
 import com.pm.pmproject.R;
 
 public class NewWorkoutActivity extends AppCompatActivity {
+    private Chronometer chronometer;
+    private boolean running;
+    private long pauseOffset;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_workout);
+
+        chronometer = findViewById(R.id.chronometer);
+        chronometer.setFormat("%s");
+        chronometer.setBase(SystemClock.elapsedRealtime());
+    }
+
+    public void startWorkout(View v){
+        if(!running){
+            chronometer.setBase(SystemClock.elapsedRealtime()-pauseOffset);
+            chronometer.start();
+            running=true;
+
+        }
+    }
+
+    public void pauseWorkout(View v){
+        if(running){
+            chronometer.stop();
+            pauseOffset = SystemClock.elapsedRealtime()-chronometer.getBase();
+            running=false;
+        }
+    }
+
+    public void stopWorkout(View v){
+        //tu zatrzymanie czasu i przejscie do zapisania treningu
     }
 }
