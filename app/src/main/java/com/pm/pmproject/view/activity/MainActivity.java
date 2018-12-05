2ae01cdaa023639e60ab381e58527392ac505e24
ijuiju71 <m.lapa45@gmail.com>
2018-12-05 18:52:03 +0100

    Side menu "Main menu" translate

--- app/src/main/java/com/pm/pmproject/view/activity/MainActivity.java
+++ app/src/main/java/com/pm/pmproject/view/activity/MainActivity.java
@@ -1,10 +1,14 @@
 package com.pm.pmproject.view.activity;
 
 import android.content.Intent;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import com.pm.pmproject.R;
@@ -46,6 +50,7 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intentTrainingHistory);
             }
         });
+
 
         // enable logs for debugging purposes
         QueryBuilder.LOG_SQL = true;
